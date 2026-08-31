0daee2933c002abe38eed58ea3a1b63695fffab3
BufaloAcquatico <alessiolentini@gmail.com>
2026-08-31 23:10:48 +0200

    Initial commit

---
+++ bcgsmLoader.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         BCGagSoundMod Loader
+// @namespace    https://www.bondageprojects.com/
+// @version      1
+// @description  BC extension adding gag related sounds for a miscellanous of situations.
+// @author       BufaloAcquatico
+// @downloadURL  https://raw.githubusercontent.com/BufaloAcquatico/BCGagSoundMod/main/bcgsmLoader.user.js
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.src = "https://raw.githubusercontent.com/BufaloAcquatico/BCGagSoundMod/main/bcgsm.js";
+    document.head.appendChild(script);
+})();
